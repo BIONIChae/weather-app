e085691a59a736b1c8479b08623420a1448c896e
Chaeee <89851362+BIONIChae@users.noreply.github.com>
2023-08-13 17:39:09 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
 let now = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-let day = days[now.getDay()];
 let hours = now.getHours();
 let mins = now.getMinutes();
 let h2 = document.querySelector("h2");
@@ -21,21 +10,40 @@ if (hours < 10) {
   hours = hours.toString().padStart(2, "0");
 }
 
-if (hours < 23 && hours > 18) {
+if (hours >= 18 && hours <= 23) {
   let backdrop = document.querySelector(".container");
   let tempColor = document.querySelector("#temperature");
   let button = document.querySelector("#button");
   let search = document.querySelector("#display-city");
-  backdrop.style.background = `url("https://dthezntil550i.cloudfront.net/5b/latest/5b2110212135019430022214799/de7639eb-1734-4ec1-802d-4eb0454055a1.png")`;
+  let column = document.querySelector(".col-2");
+  let moon = document.querySelector(".weather");
+  let cityName = document.querySelector("#city-name");
+  let dateTime = document.querySelector("#time-date");
+  let maxTemp = document.querySelector(".max");
+  let minTemp = document.querySelector(".min");
+  backdrop.style.background = `url("https://4kwallpapers.com/images/wallpapers/full-moon-forest-night-dark-starry-sky-5k-8k-2560x2560-1684.jpg")`;
   backdrop.style.backgroundSize = "cover";
   symbol.setAttribute(
     "src",
     `https://em-content.zobj.net/source/telegram/358/full-moon_1f315.webp`
   );
-  tempColor.style.color = `rgb(38, 0, 125)`;
+  tempColor.style.color = `rgb(255, 242, 0)`;
   button.style.backgroundColor = `black`;
   search.style.backgroundColor = `grey`;
   search.style.color = "white";
+  column.style.backgroundImage = `linear-gradient(
+    to bottom,
+    rgb(0, 7, 136),
+    rgb(0, 0, 0)
+  )`;
+  moon.setAttribute(
+    "src",
+    "https://em-content.zobj.net/source/twitter/348/new-moon_1f311.png"
+  );
+  cityName.style.color = `white`;
+  dateTime.style.color = `white`;
+  minTemp.style.color = `white`;
+  maxTemp.style.color = `white`;
 }
 h2.innerHTML = `${day} | ${hours}:${mins}`;
 
@@ -64,7 +72,6 @@ function showTemp(response) {
       `https://em-content.zobj.net/source/microsoft-teams/363/sun-behind-rain-cloud_1f326-fe0f.png`
     );
   }
-
   if (
     response.data.weather[0].icon === "01n" &&
     response.data.weather[0].description === "clear sky"
@@ -73,13 +80,15 @@ function showTemp(response) {
       "src",
       `https://em-content.zobj.net/source/telegram/358/full-moon_1f315.webp`
     );
-  } else if (response.data.weather[0].description === "clear sky") {
+  } else if (
+    response.data.weather[0].description === "clear sky" &&
+    response.data.weather[0].icon === "01d"
+  ) {
     symbol.setAttribute(
       "src",
       `https://em-content.zobj.net/source/microsoft-teams/363/sun_2600-fe0f.png`
     );
   }
-  
   if (response.data.weather[0].description === "few clouds") {
     symbol.setAttribute(
       "src",
@@ -98,10 +107,14 @@ function showTemp(response) {
   if (response.data.weather[0].description === "broken clouds") {
     symbol.setAttribute(
       "src",
-      `https://em-content.zobj.net/source/microsoft-teams/363/sun-behind-large-cloud_1f325-fe0f.png`
+      `https://em-content.zobj.net/source/microsoft-teams/363/cloud_2601-fe0f.png`
     );
   }
-  if (response.data.weather[0].description === "haze") {
+  if (
+    response.data.weather[0].description === "haze" ||
+    response.data.weather[0].description === "mist" ||
+    response.data.weather[0].description === "fog"
+  ) {
     symbol.setAttribute(
       "src",
       `https://em-content.zobj.net/source/microsoft-teams/363/fog_1f32b-fe0f.png`
@@ -130,10 +143,18 @@ function showTemp(response) {
   let humidity = document.querySelector("#humidity");
 
   description.innerHTML = response.data.weather[0].description;
-  windspeed.innerHTML = `Wind: ${response.data.wind.speed}`;
-  humidity.innerHTML = `Humidity: ${response.data.main.humidity}`;
+  windspeed.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
+  humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  getForecast(response.data.coord);
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "6782253072f7d90462731a624097fc54";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(weeklyForecast);
+}
 function handlePosition(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
@@ -151,6 +172,8 @@ locationButton.addEventListener("click", getCurrentLocationWeather);
 
 function convertToCels(event) {
   event.preventDefault();
+  celsius.classList.add("active");
+  fahrenheit.classList.remove("active");
   let temp = document.querySelector("#temperature");
   temp.innerHTML = Math.round(celsiusTemp);
 }
@@ -159,6 +182,8 @@ celsius.addEventListener("click", convertToCels);
 
 function convertToFahr(event) {
   event.preventDefault();
+  fahrenheit.classList.add("active");
+  cels.classList.remove("active");
   let temper = document.querySelector("#temperature");
   let displayfahr = Math.round((celsiusTemp * 9) / 5 + 32);
   temper.innerHTML = displayfahr;
@@ -168,9 +193,36 @@ fahrenheit.addEventListener("click", convertToFahr);
 
 let celsiusTemp = null;
 
-function weeklyForecast(list) {
-  let days = list.temp.day;
-  let day = document.querySelector("#text");
-  day.innerHTML = days;
+function weeklyForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index > 0 && index < 9) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+          <img
+          class="weather"
+          src="https://em-content.zobj.net/source/joypixels-animations/366/sun_2600-fe0f.gif"
+          alt="sun"
+          width="60"
+        />
+        <div class="temps">
+          <span class="max">${forecastDay.temp.max}°</span>
+          <span class="min">${forecastDay.temp.min}°</span>
+        </div>
+      </div>`;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
+function formatDay(timestamp) {
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let date = new Date(timestamp * 1000);
+  let day = days[date.getDay()];
+
+  return days[day];
+}
