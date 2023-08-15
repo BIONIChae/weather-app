61b51ad0d8491f189eee4a00e37e97510e5aac68
Chaeee <89851362+BIONIChae@users.noreply.github.com>
2023-08-15 17:24:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -163,7 +163,7 @@ function showTemp(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "6782253072f7d90462731a624097fc54";
+  let apiKey = "a969311cfcbb4a83dfad2cf7478397f9";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
   axios.get(apiUrl).then(weeklyForecast);
@@ -218,7 +218,9 @@ function weeklyForecast(response) {
           <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
           <img
           class="weather"
-          src="https://em-content.zobj.net/source/joypixels-animations/366/sun_2600-fe0f.gif"
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
           alt="sun"
           width="60"
         />
